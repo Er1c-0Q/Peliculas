dc3074fca0f1698846a921111c92bd3c217de36a
Adan Eric Ojeda Quintero <124117640+Er1c-0Q@users.noreply.github.com>
2023-03-21 04:51:59 +0000

    constructores

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -7,19 +7,59 @@ namespace Pelicula
     public class Pelicula
     {
         //Campos
-        string titulo, director, pais;
-        Int16 año;
+        private string titulo, director, pais;
+        private Int16 año;
         private List<Actor> actores = new List<Actor>();
 
         //Constructores
-     
+        public Pelicula()
+        {
+
+        }
+        public Pelicula(string newtit, Int16 newaño)
+        {
+            titulo = newtit;
+            año = newaño;
+        }
         //Métodos
+        string GetTitulo(){return titulo;}
+        string GetDirector(){return director;}
+        string GetPais(){return pais;}
+        Int16 GetAño(){return año;}
+
+        public void SetTitulo(string t)
+        {
+            titulo = t;
+        }
+        public void SetDirector(string d)
+        {
+            director = d;
+        }
+         public void SetPais(string p)
+        {
+            pais = p;
+        }
+        public void SetAño(Int16 a)
+        {
+            año = a;
+        }
      
         public void Imprime()
         {
-           
-          Console.WriteLine("Titanic");
-          Console.WriteLine(2004);
+          Console.WriteLine(titulo);
+          Console.WriteLine(director);
+          Console.WriteLine(pais);
+          Console.WriteLine(año);
+          System.Console.WriteLine();
+          
+        }
+        public void Imprime2()
+        {
+          Console.WriteLine(titulo);
+          Console.WriteLine(director);
+          Console.WriteLine(pais);
+          Console.WriteLine(año);
+          System.Console.WriteLine();
           
         }
 
@@ -49,9 +89,19 @@ namespace Pelicula
 
         static void Main(string[] args)
         {
-            Pelicula tituaño = new Pelicula ();
-            
-            tituaño.Imprime();
+            Pelicula Agua = new Pelicula ();
+            Agua.SetTitulo("La Forma del Agua");
+            Agua.SetDirector("Gillermo del Toro");
+            Agua.SetPais("Estados Unidos");
+            Agua.SetAño(2017);
+            Agua.Imprime();
+
+            Pelicula Tit = new Pelicula();
+            Agua.SetTitulo("Titanic");
+            Agua.SetDirector("James Cameron");
+            Agua.SetPais("Estados Unidos");
+            Agua.SetAño(1997);
+            Agua.Imprime2();
 
         }
     }
